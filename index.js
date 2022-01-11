ccd71cc443052cd30047c9052190cdedc8807581
Jesus Díaz Rivas <jesus20.11@hotmail.es>
2022-01-11 13:36:12 +0100

    Update: Part2, Literal object, some behaviour

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//@ts-check
 /**
  * @author Jesús Díaz Rivas
  *
@@ -5,79 +6,92 @@
  * Partiendo de un documento html vacío, crea los elementos HTML de una calculadora mediante los métodos del
  *  objeto predefinido document.
  *  */
-window.addEventListener("DOMContentLoaded", () => {
-    //Crear elementos
-    let display = document.createElement("input");
-    display.setAttribute("type", "number");
-    display.setAttribute("id", "display");
-    display.setAttribute("name", "display");
-    display.setAttribute("value", "0");
-    
-    let btnCE = document.createElement("button");
-    let btnBorrar = document.createElement("button");
-    let btnModulo = document.createElement("button");
-    let btnSuma = document.createElement("button");
-    let btnResta = document.createElement("button");
-    let btnMultiplicacion = document.createElement("button");
-    let btnDivision = document.createElement("button");
-    let btnIgual = document.createElement("button");
-    let btnPunto = document.createElement("button");
-    let btn0 = document.createElement("button");
-    let btn1 = document.createElement("button");
-    let btn2 = document.createElement("button");
-    let btn3 = document.createElement("button");
-    let btn4 = document.createElement("button");
-    let btn5 = document.createElement("button");
-    let btn6 = document.createElement("button");
-    let btn7 = document.createElement("button");
-    let btn8 = document.createElement("button");
-    let btn9 = document.createElement("button");   
-    let btnMasMenos = document.createElement("button");
-    let wrapper = document.createElement("div");
-    //Añadir elementos
-    wrapper.appendChild(display);
-    wrapper.appendChild(btnCE);
-    wrapper.appendChild(btnBorrar);
-    wrapper.appendChild(btnModulo);
-    wrapper.appendChild(btnSuma);
-    wrapper.appendChild(btn7);
-    wrapper.appendChild(btn8);
-    wrapper.appendChild(btn9);
-    wrapper.appendChild(btnResta);
-    wrapper.appendChild(btn4);
-    wrapper.appendChild(btn5);
-    wrapper.appendChild(btn6);
-    wrapper.appendChild(btnMultiplicacion);
-    wrapper.appendChild(btn1);
-    wrapper.appendChild(btn2);
-    wrapper.appendChild(btn3);
-    wrapper.appendChild(btnDivision);
-    wrapper.appendChild(btn0);
-    wrapper.appendChild(btnMasMenos);
-    wrapper.appendChild(btnPunto);
-    wrapper.appendChild(btnIgual);
-    
-    document.body.appendChild(wrapper);
-    //Añadir contenido 
-    btnCE.innerHTML = "CE";
-    btnBorrar.innerHTML = "←";
-    btnModulo.innerHTML = "%";
-    btnSuma.innerHTML = "+";
-    btn7.innerHTML = "7";
-    btn8.innerHTML = "8";
-    btn9.innerHTML = "9";
-    btnResta.innerHTML = "-";
-    btn4.innerHTML = "4";
-    btn5.innerHTML = "5";
-    btn6.innerHTML = "6";
-    btnMultiplicacion.innerHTML = "x";
-    btn1.innerHTML = "1";
-    btn2.innerHTML = "2";
-    btn3.innerHTML = "3";
-    btnDivision.innerHTML = "/";
-    btn0.innerHTML = "0";
-    btnMasMenos.innerHTML = "+/-";
-    btnPunto.innerHTML = ".";
-    btnIgual.innerHTML = "=";
-    
-});
+
+const myCalculator = {
+    // Variables
+    buttons: [
+        'CE',
+        '←',
+        '%',
+        '+',
+        '7',
+        '8',
+        '9',
+        '-',
+        '4',
+        '5',
+        '6',
+        'x',
+        '1',
+        '2',
+        '3',
+        '÷',
+        '0',
+        '±',
+        ',',
+        '=',
+    ],
+    init() {
+        window.addEventListener('DOMContentLoaded', () => {
+            let display = document.createElement('input');
+            display.setAttribute('type', 'text');
+            display.setAttribute('id', 'display');
+            display.setAttribute('name', 'display');
+            display.setAttribute('value', '0');
+            this.wrapper = document.createElement('div');
+            this.display = display;
+
+            this.wrapper.appendChild(this.display);
+            this.buttons.forEach((button) => {
+                let btn = document.createElement('button');
+                btn.setAttribute('id', button);
+                btn.setAttribute('value', button);
+                btn.innerHTML = button;
+                this.wrapper.appendChild(btn);
+            });
+            document.body.appendChild(this.wrapper);
+
+            this.initBehaviour();
+        });
+    },
+    initBehaviour() {
+            // Inicialmente en el display aparece el cero sin decimal.
+            // En el display sólo puede aparecer un punto decimal.
+            // A la izquierda del punto sólo puede aparecer un cero ("00.1" no es válido).
+            // No hay que escribir "0." para que te acepte el decimal. Basta con que pulse la coma decimal. Entonces el resto se consideran decimales.
+            // En el display siempre ha de haber un dígito. En caso de usar el retroceso y ser el último carácter aparecerá un cero.
+            // El cero negativo no existe ("-0" no es válido)
+        this.wrapper.childNodes.forEach((element) => {
+            switch (true) {
+                case /[0-9]/g.test(element.id):
+                    element.addEventListener('click', () => {
+                        this.display.value = this.display.value === '0' ? element.id : this.display.value + element.id;
+                    });
+                    break;
+                case element.id ==='CE':
+                    element.addEventListener('click', () => {
+                        this.display.value = '0';
+                    });
+                    break;
+                case element.id ==='←':
+                    element.addEventListener('click', () => {
+                        this.display.value = this.display.value.length > 1 ? this.display.value.slice(0, -1) : '0';
+                    });
+                    break;
+                case element.id ===',':
+                    element.addEventListener('click', (e) => {
+                        if (this.display.value.includes(',')) {
+                            e.preventDefault();
+                        };
+                        this.display.value = this.display.value.concat(element.id);
+                    });
+                    break;
+                case element.id ==='±':
+                    element.addEventListener('click', () => {
+                        this.display.value = parseInt(this.display.value) * -1;
+                    });
+            }
+        });
+    },
+};
+myCalculator.init();
