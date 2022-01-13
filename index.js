64eb7157e86c13bb92b8f4bb77ea8f40edb22dc0
Jesus Díaz Rivas <jesus20.11@hotmail.es>
2022-01-13 15:05:55 +0100

    Refactor: eventListeners

--- index.js
+++ index.js
@@ -36,116 +36,116 @@
             '=',
         ],
         init: () => {
-            window.addEventListener('DOMContentLoaded', () => {
-                let display = document.createElement('input');
-                display.setAttribute('type', 'text');
-                display.setAttribute('id', 'display');
-                display.setAttribute('name', 'display');
-                display.setAttribute('value', '0');
-                myCalculator.wrapper = document.createElement('div');
-                myCalculator.display = display;
+            //No se puede usar this en una función arrow por mucho que metas un bind.
+            let display = document.createElement('input');
+            display.setAttribute('type', 'text');
+            display.setAttribute('id', 'display');
+            display.setAttribute('name', 'display');
+            display.setAttribute('value', '0');
+            myCalculator.wrapper = document.createElement('div');
+            myCalculator.display = display;
 
-                myCalculator.wrapper.appendChild(myCalculator.display);
-                myCalculator.buttons.forEach((button) => {
-                    let btn = document.createElement('button');
-                    btn.setAttribute('id', button);
-                    btn.setAttribute('value', button);
-                    btn.innerHTML = button;
-                    myCalculator.wrapper.appendChild(btn);
-                });
-                document.body.appendChild(myCalculator.wrapper);
+            display.addEventListener('keydown', myCalculator.displayBehaviour);
 
-                myCalculator.initBehaviour();
+            myCalculator.wrapper.appendChild(myCalculator.display);
+
+            myCalculator.buttons.forEach((button) => {
+                let btn = document.createElement('button');
+                btn.setAttribute('id', button);
+                btn.setAttribute('value', button);
+                btn.innerHTML = button;
+                myCalculator.wrapper.appendChild(btn);
+                console.log(button);
+                btn.addEventListener('click', myCalculator.addBehaviour(button));
             });
+            document.body.appendChild(myCalculator.wrapper);
+
+            myCalculator.initBehaviour();
         },
-        initBehaviour: () => {
-            myCalculator.wrapper.childNodes.forEach((element) => {
-                switch (true) {
-                    case element.name === 'display':
-                        element.addEventListener('keydown', (e) => {
-                            if (e.key === 'Backspace' && myCalculator.display.value.length === 1) {
-                                myCalculator.display.value = '0';
-                                e.preventDefault();
-                            }
-                            if (/\D/.test(e.key) && e.key !== 'Backspace') {
-                                e.preventDefault();
-                            }
-                            if (/\d/.test(e.key) && myCalculator.display.value === '0') {
-                                myCalculator.display.value = myCalculator.display.value.slice(2);
-                            }
-                        });
-                        break;
-                    case /[0-9]/g.test(element.value):
-                        element.addEventListener('click', () => {
-                            if (myCalculator.operating) {
-                                myCalculator.operating = false;
-                                myCalculator.display.value = element.value;
-                                
-                            } else {
-                                myCalculator.display.value =
-                                    myCalculator.display.value === '0'
-                                        ? element.value
-                                        : myCalculator.display.value + element.value;
-                            }
-                        });
-                        break;
-                    case element.value === 'CE':
-                        element.addEventListener('click', () => {
+        addBehaviour: (buttonContent) => {
+            switch (buttonContent) {
+                case 'CE':
+                    return () => {
+                        myCalculator.display.value = '0';
+                        myCalculator.reset();
+                    };
+                case '←':
+                    return () => {
+                        console.log('hola');
+                        const displayValue = myCalculator.display.value;
+                        console;
+                        let decreaseFactor = -1;
+                        let limitFactor = 1;
+                        if (displayValue.includes(',') && displayValue.split(',')[1].length === 1) {
+                            decreaseFactor--;
+                        }
+                        if (displayValue.includes('-')) {
+                            limitFactor++;
+                        }
+                        myCalculator.display.value =
+                            displayValue.length <= limitFactor
+                                ? '0'
+                                : displayValue.slice(0, decreaseFactor);
+                        if (myCalculator.display.value === '-0') {
                             myCalculator.display.value = '0';
-                            myCalculator.reset();
-                        });
-                        break;
-                    case element.value === '←':
-                        element.addEventListener('click', () => {
-                            const value = myCalculator.display.value;
-                            let decreaseFactor = -1;
-                            let limitFactor = 1;
-                            if (value.includes(',') && value.split(',')[1].length === 1) {
-                                decreaseFactor--;
-                            }
-                            if (value.includes('-')) {
-                                limitFactor++;
-                            }
+                        }
+                    };
+                case '%':
+                case '+':
+                case '-':
+                case 'x':
+                case '÷':
+                    return () => {
+                        if (myCalculator.operator) {
+                            myCalculator.operate();
+                        }
+                        myCalculator.operand1 = myCalculator.display.value;
+                        myCalculator.operator = buttonContent;
+                        myCalculator.operating = true;
+                    };
+
+                case '=':
+                    return myCalculator.operate;
+                case '±':
+                    return () => {
+                        if (myCalculator.display.value === '0') return;
+                        myCalculator.display.value = myCalculator.display.value.includes('-')
+                            ? myCalculator.display.value.slice(1)
+                            : `-${myCalculator.display.value}`;
+                    };
+                case ',':
+                    return () => {
+                        if (!myCalculator.display.value.includes(',')) {
                             myCalculator.display.value =
-                                value.length <= limitFactor ? '0' : value.slice(0, decreaseFactor);
-                            if (myCalculator.display.value === '-0') {
-                                myCalculator.display.value = '0';
-                            }
-                        });
-                        break;
-                    case element.value === ',':
-                        element.addEventListener('click', () => {
-                            if (!myCalculator.display.value.includes(',')) {
-                                myCalculator.display.value = myCalculator.display.value.concat(
-                                    element.value
-                                );
-                            }
-                        });
-                        break;
-                    case element.value === '±':
-                        element.addEventListener('click', () => {
-                            if (myCalculator.display.value === '0') return;
-                            myCalculator.display.value = myCalculator.display.value.includes('-')
-                                ? myCalculator.display.value.slice(1)
-                                : `-${myCalculator.display.value}`;
-                        });
-                        break;
-                    case /[\+\-\x\÷\%]/g.test(element.value):
-                        element.addEventListener('click', () => {
-                            if (myCalculator.operator) {
-                                myCalculator.operate();
-                            }
-                            myCalculator.operand1 = myCalculator.display.value;
-                            myCalculator.operator = element.value;
-                            myCalculator.operating = true;
-                            
-                        });
-                        break;
-                    case element.value === '=':
-                        element.addEventListener('click', myCalculator.operate);
-                        break;
-                }
-            });
+                                myCalculator.display.value.concat(buttonContent);
+                        }
+                    };
+                default:
+                    return () => {
+                        console.log(buttonContent);
+                        if (myCalculator.operating) {
+                            myCalculator.operating = false;
+                            myCalculator.display.value = buttonContent;
+                        } else {
+                            myCalculator.display.value =
+                                myCalculator.display.value === '0'
+                                    ? buttonContent
+                                    : myCalculator.display.value + buttonContent;
+                        }
+                    };
+            }
+        },
+        displayBehaviour: (e) => {
+            if (e.key === 'Backspace' && myCalculator.display.value.length === 1) {
+                myCalculator.display.value = '0';
+                e.preventDefault();
+            }
+            if (/\D/.test(e.key) && e.key !== 'Backspace') {
+                e.preventDefault();
+            }
+            if (/\d/.test(e.key) && myCalculator.display.value === '0') {
+                myCalculator.display.value = myCalculator.display.value.slice(2);
+            }
         },
         calculate: () => {
             let result = 0;
@@ -188,7 +188,7 @@
             myCalculator.operand2 = undefined;
             myCalculator.operator = undefined;
             myCalculator.operating = false;
-        }
+        },
     };
-    myCalculator.init();
+    window.addEventListener('DOMContentLoaded', myCalculator.init.bind(myCalculator));
 }
