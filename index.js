b9f5adc82f6567d62ee54699882a929458ce6893
Jesus Díaz Rivas <jesus20.11@hotmail.es>
2022-01-18 09:38:24 +0100

    fix #2, fix #3, fix #4,fix #5,fix #6, fix #7

--- index.js
+++ index.js
@@ -14,86 +14,85 @@
         operator: undefined,
         operating: false,
         buttons: [
-            'CE',
-            '←',
-            '%',
-            '+',
-            '7',
-            '8',
-            '9',
-            '-',
-            '4',
-            '5',
-            '6',
-            'x',
-            '1',
-            '2',
-            '3',
-            '÷',
-            '0',
-            '±',
-            ',',
-            '=',
+            "CE",
+            "←",
+            "%",
+            "+",
+            "7",
+            "8",
+            "9",
+            "-",
+            "4",
+            "5",
+            "6",
+            "x",
+            "1",
+            "2",
+            "3",
+            "÷",
+            "0",
+            "±",
+            ",",
+            "=",
         ],
         init: () => {
-            //No se puede usar this en una función arrow por mucho que metas un bind.
-            let display = document.createElement('input');
-            display.setAttribute('type', 'text');
-            display.setAttribute('id', 'display');
-            display.setAttribute('name', 'display');
-            display.setAttribute('value', '0');
-            myCalculator.wrapper = document.createElement('div');
+            let display = document.createElement("input");
+            display.setAttribute("type", "text");
+            display.setAttribute("id", "display");
+            display.setAttribute("name", "display");
+            display.setAttribute("value", "0");
+            myCalculator.wrapper = document.createElement("div");
+            myCalculator.addWrapperCss();
             myCalculator.display = display;
+            myCalculator.addDisplayCss();
 
-            display.addEventListener('keydown', myCalculator.displayBehaviour);
+            display.addEventListener("keydown", myCalculator.displayBehaviourBeforeDisplayChange);
+            display.addEventListener("input", myCalculator.displayBehaviourAfterDisplayChange);
 
             myCalculator.wrapper.appendChild(myCalculator.display);
 
             myCalculator.buttons.forEach((button) => {
-                let btn = document.createElement('button');
-                btn.setAttribute('id', button);
-                btn.setAttribute('value', button);
+                let btn = document.createElement("button");
+                btn.setAttribute("id", button);
+                btn.setAttribute("value", button);
                 btn.innerHTML = button;
+                myCalculator.addBtnCss(btn);
                 myCalculator.wrapper.appendChild(btn);
-                console.log(button);
-                btn.addEventListener('click', myCalculator.addBehaviour(button));
+                btn.addEventListener("click", myCalculator.addBehaviour(button));
             });
             document.body.appendChild(myCalculator.wrapper);
-
-            myCalculator.initBehaviour();
         },
         addBehaviour: (buttonContent) => {
             switch (buttonContent) {
-                case 'CE':
+                case "CE":
                     return () => {
-                        myCalculator.display.value = '0';
+                        myCalculator.display.value = "0";
                         myCalculator.reset();
                     };
-                case '←':
+                case "←":
                     return () => {
                         const displayValue = myCalculator.display.value;
-                        console;
                         let decreaseFactor = -1;
                         let limitFactor = 1;
-                        if (displayValue.includes(',') && displayValue.split(',')[1].length === 1) {
+                        if (displayValue.includes(",") && displayValue.split(",")[1].length === 1) {
                             decreaseFactor--;
                         }
-                        if (displayValue.includes('-')) {
+                        if (displayValue.includes("-")) {
                             limitFactor++;
                         }
                         myCalculator.display.value =
                             displayValue.length <= limitFactor
-                                ? '0'
+                                ? "0"
                                 : displayValue.slice(0, decreaseFactor);
-                        if (myCalculator.display.value === '-0') {
-                            myCalculator.display.value = '0';
+                        if (myCalculator.display.value === "-0") {
+                            myCalculator.display.value = "0";
                         }
                     };
-                case '%':
-                case '+':
-                case '-':
-                case 'x':
-                case '÷':
+                case "%":
+                case "+":
+                case "-":
+                case "x":
+                case "÷":
                     return () => {
                         if (myCalculator.operator) {
                             myCalculator.operate();
@@ -103,81 +102,90 @@
                         myCalculator.operating = true;
                     };
 
-                case '=':
+                case "=":
                     return myCalculator.operate;
-                case '±':
+                case "±":
                     return () => {
-                        if (myCalculator.display.value === '0') return;
-                        myCalculator.display.value = myCalculator.display.value.includes('-')
+                        if (myCalculator.display.value === "0") return;
+                        myCalculator.display.value = myCalculator.display.value.includes("-")
                             ? myCalculator.display.value.slice(1)
                             : `-${myCalculator.display.value}`;
                     };
-                case ',':
+                case ",":
                     return () => {
-                        if (!myCalculator.display.value.includes(',')) {
+                        if (!myCalculator.display.value.includes(",")) {
                             myCalculator.display.value =
                                 myCalculator.display.value.concat(buttonContent);
                         }
                     };
                 default:
                     return () => {
-                        console.log(buttonContent);
                         if (myCalculator.operating) {
                             myCalculator.operating = false;
                             myCalculator.display.value = buttonContent;
                         } else {
                             myCalculator.display.value =
-                                myCalculator.display.value === '0'
+                                myCalculator.display.value === "0"
                                     ? buttonContent
                                     : myCalculator.display.value + buttonContent;
                         }
                     };
             }
         },
-        displayBehaviour: (e) => {
-            if (e.key === 'Backspace' && myCalculator.display.value.length === 1) {
-                myCalculator.display.value = '0';
+        displayBehaviourBeforeDisplayChange: (e) => {
+            if (e.key === "Backspace" && myCalculator.display.value.length === 1) {
+                myCalculator.display.value = "0";
                 e.preventDefault();
             }
-            if (/\D/.test(e.key) && e.key !== 'Backspace') {
+            if (/\D/.test(e.key) && e.key !== "Backspace") {
                 e.preventDefault();
             }
-            if (/\d/.test(e.key) && myCalculator.display.value === '0') {
+            if (/\d/.test(e.key) && myCalculator.display.value === "0") {
                 myCalculator.display.value = myCalculator.display.value.slice(2);
+            }
+        },
+        displayBehaviourAfterDisplayChange: () => {
+            //I don't know if 0 will be right or left when e.key === '0' is pressed, so I will control it here.
+            if (
+                myCalculator.display.value !== "0" &&
+                myCalculator.display.value.indexOf("0") === 0
+            ) {
+                myCalculator.display.value = myCalculator.display.value.slice(1);
             }
         },
         calculate: () => {
             let result = 0;
-            switch (myCalculator.operator) {
-                case '+':
-                    result = parseFloat(myCalculator.operand1) + parseFloat(myCalculator.operand2);
+            const { operand1, operand2, operator } = myCalculator;
+            switch (operator) {
+                case "+":
+                    result = operand1 + operand2;
                     break;
-                case '-':
-                    result = parseFloat(myCalculator.operand1) - parseFloat(myCalculator.operand2);
+                case "-":
+                    result = operand1 - myCalculator.operand2;
                     break;
-                case 'x':
-                    result = parseFloat(myCalculator.operand1) * parseFloat(myCalculator.operand2);
+                case "x":
+                    result = operand1 * operand2;
                     break;
-                case '÷':
-                    result = parseFloat(myCalculator.operand1) / parseFloat(myCalculator.operand2);
+                case "÷":
+                    result = operand1 / operand2;
                     break;
-                case '%':
-                    result = parseFloat(myCalculator.operand1) % parseFloat(myCalculator.operand2);
+                case "%":
+                    result = operand1 % operand2;
             }
             return result;
         },
         operate: () => {
             if (myCalculator.operator && !myCalculator.operating) {
                 myCalculator.operand2 = myCalculator.display.value;
-                myCalculator.operand1 = myCalculator.operand1.replace(',', '.');
-                myCalculator.operand2 = myCalculator.operand2.replace(',', '.');
+                myCalculator.operand1 = parseFloat(myCalculator.operand1.replace(",", "."));
+                myCalculator.operand2 = parseFloat(myCalculator.operand2.replace(",", "."));
                 const result = myCalculator.calculate();
 
                 myCalculator.display.value = isNaN(result)
-                    ? '0' //0/0
-                    : result.toString().replace('.', ',');
+                    ? "0" //0/0
+                    : result.toString().replace(".", ",");
                 if (myCalculator.display.value.length > 10) {
-                    myCalculator.display.style.fontSize = '1em';
+                    myCalculator.display.style.fontSize = "1em";
                 }
                 myCalculator.reset();
             }
@@ -188,6 +196,32 @@
             myCalculator.operator = undefined;
             myCalculator.operating = false;
         },
+        addWrapperCss: () => {
+            myCalculator.wrapper.style.cssText = `
+            justify-content: center;
+            box-shadow: 0px 0px 5px;
+            height: 396px;
+            padding: 20px;
+            width: 301px;
+            grid-auto-flow: row;
+            display: grid;
+            grid-template-columns: repeat(4, 1fr);
+            grid-gap: 15px;
+            background-color: #EEEEEE;`;
+        },
+        addDisplayCss: () => {
+            myCalculator.display.style.cssText = `
+            grid-area: 1/1/1/5;
+            text-align: right;
+            font-size: 2em;
+            `;
+        },
+        addBtnCss: (btn) =>{
+           btn.style.cssText = `
+            background: linear-gradient(180deg, rgba(0, 0, 0, 0) 50%, rgb(188 188 188 / 38%) 50%);
+            font-size: 2em;
+            `;
+        }
     };
-    window.addEventListener('DOMContentLoaded', myCalculator.init.bind(myCalculator)); //El bind no sirve nde nada si init es arrow, y como todas deben ser arrow pues... Lo dejo ahí para que no se me olvide comentarlo en clase
+    window.addEventListener("DOMContentLoaded", myCalculator.init);
 }
